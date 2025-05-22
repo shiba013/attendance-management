2aac809aa7fa77749de6234bcd39b7b5469433df
reimi <r.yamada013@icloud.com>
2025-05-23 08:05:48 +0900

    Fortify導入

---
+++ src/app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Contracts\LogoutResponse;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse
+        {
+            public function toResponse($request)
+            {
+                return redirect('/login');
+            }
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+
+        RateLimiter::for('login', function (Request $request)
+        {
+            $email = (string) $request->email;
+            return Limit::perMinute(10)->by($email . $request->ip());
+        });
+    }
+}
